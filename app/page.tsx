59e5af9644758d08485a8e860b93c30913b80668
Ryan Drachenberg <tssinvestments@gmail.com>
2024-11-13 18:08:03 -0500

    fixed google translation popup on every render

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { Qr } from './components/Qr';
 
 export default function Home() {
   
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <meta name="google" content="notranslate" />
       <div className="relative flex place-items-center ">
         <Qr />
       </div>
