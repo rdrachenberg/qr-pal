1aec789fe8d7dd79471b5bedba74c5bbff69def9
Ryan Drachenberg <tssinvestments@gmail.com>
2024-11-15 15:16:39 -0500

    added link shortener functionality. Front end route /shorty. API call runs on /api/shorty

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, request, send_file, jsonify, request, render_template_s
 import qrcode
 from io import BytesIO
 import base64
+import pyshorteners
 
 app = Flask(__name__)
 
@@ -35,9 +36,29 @@ def qr_code_api():
 
     html = f"""data:image/png;base64,{img_base64}"""
     
-    print(render_template_string(html))
+    # print(render_template_string(html))
     # Return the image directly to the browser
     return render_template_string(html)
 
+def generate_short_url(long_url):
+  s = pyshorteners.Shortener()
+  return s.tinyurl.short(long_url)
+
+@app.route('/api/shorty', methods=['POST'])
+def shorten_link_api():
+    # make data object from JSON request
+    data = request.get_json()
+    # print(data)
+    
+    # get the link key and store the value
+    long_url = data.get('link')
+    # print(long_url)
+
+    short_url = generate_short_url(long_url)
+    # print(short_url)
+    
+    return render_template_string(short_url)
+
 if __name__ == '__main__':
+    app.app_context().push()
     app.run(debug=True)
---
+++ app/shorty/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from "react"
+import { Header } from "../components/Header";
+import { Footer } from "../components/Fooler";
+
+export default function Shorty() {
+    const [linkToShorten, setLinkToShorten] = useState('');
+    const [shortenedLink, setShortenedLink] = useState('');
+
+    const bodyObject = JSON.stringify({
+        link: linkToShorten
+    });
+
+    async function handleShortenClick() {
+        const request = await fetch('/api/shorty', {
+            method: 'POST',
+            body: bodyObject,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const response = await request.text()
+        console.log(response)
+        setShortenedLink(response);
+    }
+
+    return (
+        <div>
+            <Header />
+            <div className='flex min-h-screen flex-col bg-gradient-to-r from-blue-200 to-cyan-200 '>
+                <div className='flex flex-col justify-center align-middle items-center mt-14'>
+                {shortenedLink && <div className='text-3xl text-black rounded bg-white p-2'>{shortenedLink}</div>}
+                    <input 
+                        id="link" 
+                        name="link" 
+                        placeholder="Link to shorten" 
+                        onChange={(e) => setLinkToShorten(e.target.value)}  
+                        className='font-sans w-[300px] sm:w-[400px]  p-2 m-3 mt-8 rounded border border-gray-300 bg-light invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer text-black'
+                        required
+                        pattern=""
+                    />
+                    <button className=" w-[90%] sm:w-[25%] bg-black text-2xl text-white rounded-full p-5 hover:outline hover:outline-4 hover:outline-purple-200" onClick={handleShortenClick}>Make Short Link</button>
+                </div>
+                
+            </div>
+            <Footer />
+        </div>
+    )
+}
